6ad894808377f714e1d642e2cf8131040638f37e
Michel de Nijs <micheldenijs@mail.com>
2017-05-27 11:30:16 +0200

    Add project files.

---
+++ Domicile/Modules/IndexModule.cs
@@ -0,0 +1,15 @@
+﻿using Nancy;
+
+namespace Domicile.Modules
+{
+    public class IndexModule : NancyModule
+    {
+        public IndexModule()
+        {
+            Get["/"] = parameters =>
+            {
+                return View["index"];
+            };
+        }
+    }
+}
